cb8f471b8a138a4690a5bf00d8931d55a27f614a
Chamila de Alwis <chamila.sujeewa@gmail.com>
2020-10-29 01:12:27 +1300

    Add ability to read config from file
    For now, the URL settings are static. This would be replaced by a base
    URL and a suffix for the service discovery responses.

--- registry/go.mod
+++ registry/go.mod
@@ -2,4 +2,7 @@ module github.com/chamilad/telm
 
 go 1.14
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/gin-gonic/gin v1.6.3
+	gopkg.in/yaml.v2 v2.2.8
+)
--- registry/main.go
+++ registry/main.go
@@ -1,22 +1,60 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
 )
 
+type RegistryConfig struct {
+	Registry struct {
+		Discovery struct {
+			Modules  string `yaml:"modules"`
+			Login    string `yaml:"login"`
+			Provider string `yaml:"provider"`
+		} `yaml:"discovery"`
+	} `yaml:"registry"`
+}
+
 func main() {
+	c := flag.String("c", "telm.yaml", "the configuration file to use")
+	flag.Parse()
+
+	ac, err := filepath.Abs(*c)
+	if err != nil {
+		log.Fatalf("could not find the configuration file: %s, %s\n", *c, err)
+	}
+
+	yc, err := ioutil.ReadFile(ac)
+	if err != nil {
+		log.Fatalf("could not read configuration file: %s, %s\n", ac, err)
+	}
+
+	var conf RegistryConfig
+	err = yaml.Unmarshal(yc, &conf)
+	if err != nil {
+		log.Fatalf("error while parsing configuration file: %s, %s\n", ac, err)
+	}
+
+	fmt.Printf("config: %v\n", conf)
+
 	r := gin.Default()
 	r.GET("/healthz", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"registry": "ok",
 		})
 	})
 
 	r.GET("/.well-known/terraform.json", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"modules.v1": "http://localhost:8080/modules/v1/",
+			"modules.v1": conf.Registry.Discovery.Modules,
 		})
 	})
 
-	r.Run()
+	log.Fatal(r.Run())
 }
